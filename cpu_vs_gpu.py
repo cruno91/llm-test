e5d76f7cea998086bac65faac3f9b03057d58d94
Chris Runo <chris.runo@gmail.com>
2023-12-18 21:22:04 -0500

    Check for mps and alert when it's not there.

--- cpu_vs_gpu.py
+++ cpu_vs_gpu.py
@@ -4,6 +4,10 @@ import time
 
 if torch.backends.mps.is_available():
     mps_device = torch.device("mps")
+    print ("MPS device found.")
+else:
+    print ("MPS device not found.")
+    exit()
 
 torch_rand1 = torch.rand(100, 100, 100, 100).to(mps_device)
 torch_rand2 = torch.rand(100, 100, 100, 100).to(mps_device)
