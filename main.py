49ea2fb560b412566091be85bdf0999455188954
Chris Runo <chris.runo@gmail.com>
2023-12-28 22:34:37 -0500

    Add commented example of bigram language model.

--- main.py
+++ main.py
@@ -71,3 +71,11 @@ print('inputs:')
 print(x)
 print('targets:')
 print(y)
+
+# Bigram language model.
+# x = train_data[:block_size]
+# y = train_data[1:block_size+1]
+# for t in range(block_size):
+#     context = x[:t+1]
+#     target = y[t]
+#     print("when input is", context, "target is", target)
