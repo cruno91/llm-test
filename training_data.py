668faaeafe185344bc95b822afa9344b062a6237
Chris Runo <chris.runo@gmail.com>
2024-01-06 20:54:55 -0500

    Update training data to use bpe.

--- training_data.py
+++ training_data.py
@@ -1,7 +1,7 @@
 import os
 import lzma
 from tqdm import tqdm
-
+from tokenizers.implementations import ByteLevelBPETokenizer
 
 def xz_files_in_dir(directory):
     files = []
@@ -10,11 +10,9 @@ def xz_files_in_dir(directory):
             files.append(filename)
     return files
 
-
 folder_path = "../openwebtext"
-output_file_train = "output_train.txt"
-output_file_val = "output_val.txt"
-vocab_file = "vocab.txt"
+output_file_train = "output_train2.txt"
+output_file_val = "output_val2.txt"
 
 files = xz_files_in_dir(folder_path)
 total_files = len(files)
@@ -23,29 +21,33 @@ split_index = int(total_files * 0.9)  # 90% of the files for training.
 files_train = files[:split_index]
 files_val = files[split_index:]
 
-vocab = set()
-
-# Process the training files
+# Process and concatenate the training files
 with open(output_file_train, "w") as outfile:
     for filename in tqdm(files_train, total=len(files_train)):
         file_path = os.path.join(folder_path, filename)
         with lzma.open(file_path, "rt", encoding="utf-8") as infile:
             text = infile.read()
             outfile.write(text)
-            characters = set(text)
-            vocab.update(text)
 
-# Process the validation files
+# Process and concatenate the validation files
 with open(output_file_val, "w") as outfile:
     for filename in tqdm(files_val, total=len(files_val)):
         file_path = os.path.join(folder_path, filename)
         with lzma.open(file_path, "rt", encoding="utf-8") as infile:
             text = infile.read()
             outfile.write(text)
-            characters = set(text)
-            vocab.update(text)
 
-# Write the vocabulary to a file.
-with open(vocab_file, "w", encoding="utf-8") as vfile:
-    for character in sorted(vocab):
-        vfile.write(character + "\n")
+# Initialize a tokenizer
+tokenizer = ByteLevelBPETokenizer()
+
+# Train the tokenizer on the concatenated training file
+tokenizer.train(files=[output_file_train], vocab_size=52_000, min_frequency=2, special_tokens=[
+    "<s>",
+    "<pad>",
+    "</s>",
+    "<unk>",
+    "<mask>",
+])
+
+# Save the tokenizer models - vocab and merges files
+tokenizer.save_model(".", "openwebtext_bpe")
