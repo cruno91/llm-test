59251eced9914912a0d6cf6626f7ad0fc09d1dad
Chris Runo <chris.runo@gmail.com>
2024-01-11 21:35:30 -0500

    Move common training_datav2 code to training_data.

---
+++ training_data.py
@@ -0,0 +1,32 @@
+import os
+import tqdm
+import lzma
+
+def xz_files_in_dir(directory):
+    files = []
+    for filename in os.listdir(directory):
+        if filename.endswith(".xz") and os.path.isfile(os.path.join(directory, filename)):
+            files.append(filename)
+    return files
+
+
+def process_data(folder_path):
+    files = xz_files_in_dir(folder_path)
+    total_files = len(files)
+
+    split_index = int(total_files * 0.9)  # 90% of the files for training.
+    files_train = files[:split_index]
+    files_val = files[split_index:]
+
+    return files_train, files_val
+
+def process_character_training_data(file, data_split_file, folder_path, vocab):
+    with open(file, "w") as outfile:
+        for filename in tqdm(data_split_file, total=len(data_split_file)):
+            file_path = os.path.join(folder_path, filename)
+            with lzma.open(file_path, "rt", encoding="utf-8") as infile:
+                text = infile.read()
+                outfile.write(text)
+                characters = set(text)
+                vocab.update(text)
+    return vocab
--- training_datav2.py
+++ training_datav2.py
@@ -1,49 +1,21 @@
-import os
-import lzma
-from tqdm import tqdm
-
-
-def xz_files_in_dir(directory):
-    files = []
-    for filename in os.listdir(directory):
-        if filename.endswith(".xz") and os.path.isfile(os.path.join(directory, filename)):
-            files.append(filename)
-    return files
-
+from training_data import process_data
+from training_data import process_character_training_data
 
 folder_path = "../openwebtext"
 output_file_train = "output_train.txt"
 output_file_val = "output_val.txt"
 vocab_file = "vocab.txt"
 
-files = xz_files_in_dir(folder_path)
-total_files = len(files)
-
-split_index = int(total_files * 0.9)  # 90% of the files for training.
-files_train = files[:split_index]
-files_val = files[split_index:]
+# Process and concatenate the training files
+files_train, files_val = process_data(folder_path)
 
 vocab = set()
 
 # Process the training files
-with open(output_file_train, "w") as outfile:
-    for filename in tqdm(files_train, total=len(files_train)):
-        file_path = os.path.join(folder_path, filename)
-        with lzma.open(file_path, "rt", encoding="utf-8") as infile:
-            text = infile.read()
-            outfile.write(text)
-            characters = set(text)
-            vocab.update(text)
+vocab = process_character_training_data(output_file_train, files_train, folder_path, vocab)
 
 # Process the validation files
-with open(output_file_val, "w") as outfile:
-    for filename in tqdm(files_val, total=len(files_val)):
-        file_path = os.path.join(folder_path, filename)
-        with lzma.open(file_path, "rt", encoding="utf-8") as infile:
-            text = infile.read()
-            outfile.write(text)
-            characters = set(text)
-            vocab.update(text)
+vocab = process_character_training_data(output_file_val, files_val, folder_path, vocab)
 
 # Write the vocabulary to a file.
 with open(vocab_file, "w", encoding="utf-8") as vfile:
