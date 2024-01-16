c1d7d82efe8e4ba59072bfd1f3539b693bcd5912
Chris Runo <chris.runo@gmail.com>
2024-01-15 21:44:20 -0500

    Update gptv3 to use train_model.

--- gptv3.py
+++ gptv3.py
@@ -9,6 +9,7 @@ from model_gpt import get_device
 from model_gpt import load_model
 from model_gpt import get_optimizer
 from model_gpt import write_model
+from model_gpt import train_model
 
 device = get_device()
 
@@ -56,71 +57,10 @@ def decode(token_ids):
     return tokenizer.decode(token_ids)
 
 
-# Memory map for using small snippets of text from a single file of any size.
-def get_random_chunk(split):
-    filename = "bpe_output_train.txt" if split == 'train' else "bpe_output_val.txt"
-    # Opened in binary mode.
-    with open(filename, 'rb') as f:
-        # Memory map the file. (Chunks of the file are loaded into memory as needed.)
-        with mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ) as mm:
-            # Determine the file size and a random position to start reading.
-            file_size = len(mm)
-            start_pos = random.randint(0, (file_size) - block_size * batch_size)
-
-            # Seek to the random position and read the block of text.
-            mm.seek(start_pos)
-            block = mm.read(block_size * batch_size * 10 - 1)  # Increase the multiplier as needed
-
-            # Decode the block to a string, ignoring any invalid byte sequences.
-            decoded_block = block.decode('utf-8', errors='ignore').replace('\r', '')
-
-            # Train and test splits.
-            data = torch.tensor(encode(decoded_block), dtype=torch.long)
-
-    return data
-
-
-# Get a batch of data.
-def get_batch(split):
-    # Get the data from the training or validation split.
-    while True:
-        data = get_random_chunk(split)
-        if len(data) > block_size:
-            break
-    # Get a random index.
-    ix = torch.randint(len(data) - block_size, (batch_size,))
-    # Get the data from the random index to the random index plus the block size.
-    x = torch.stack([data[i:i + block_size] for i in ix])
-    # Get the data from the random index plus 1 to the random index plus the block size plus 1.
-    y = torch.stack([data[i + 1:i + block_size + 1] for i in ix])
-    # Move the data to the device.
-    x, y = x.to(device), y.to(device)
-    return x, y
-
-
-# Estimate the training loss.
-@torch.no_grad()
-def estimate_loss():
-    # Create a dictionary to store the losses.
-    out = {}
-    # Set the model to evaluation mode.
-    model.eval()
-    for split in ["train", "val"]:
-        # Create a tensor to store the losses.
-        losses = torch.zeros(eval_iterations)
-        # Get the losses.
-        for k in range(eval_iterations):
-            # Get the batch.
-            x, y = get_batch(split)
-            # Forward pass.
-            logits, loss = model(x, y)
-            # Store the loss.
-            losses[k] = loss.item()
-        # Get the mean of the losses.
-        out[split] = losses.mean()
-    # Set the model back to training mode.
-    model.train()
-    return out
+training_data_filemap = {
+    "train": "bpe_output_train.txt",
+    "val": "bpe_output_val.txt"
+}
 
 
 model = load_model("model-03.pkl", vocab_size, device, n_embed, block_size, n_head, n_layer, dropout)
@@ -129,26 +69,7 @@ model = load_model("model-03.pkl", vocab_size, device, n_embed, block_size, n_he
 optimizer = get_optimizer(model, learning_rate)
 
 # Train the model.
-for i in range(max_iterations):
-    # Print the training loss.
-    if i % eval_iterations == 0:
-        losses = estimate_loss()
-        # We want to see convergence: Val loss should be lower than train loss.
-        print(f"step: {i}, train loss: {losses['train']:.3f}, val losses: {losses['val']:.3f}")
-
-    # Get the batch.
-    xb, yb = get_batch("train")
-    # Forward pass.
-    logits, loss = model.forward(xb, yb)
-    # Backward pass.
-    optimizer.zero_grad(set_to_none=True)
-    # Backpropagation of the loss.
-    loss.backward()
-    # Update the weights.
-    optimizer.step()
-
-# Print the loss.
-print(loss.item())
+train_model(model, max_iterations, optimizer, eval_iterations, training_data_filemap, block_size, batch_size, encode, device, multiplier=10)
 
 write_model("model-03.pkl", model)
 
